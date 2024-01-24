76ffa7bb77db97ea1173304ac325ffba0021d40c
bhaumiksonii <bhaumik0369@gmail.com>
2024-01-24 17:09:23 +0530

    updaetd

---
+++ src/components/Home/IntroScene.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="scene-intro" id="intro">
+    <div class="static-container">
+      <h1 class="title">
+        <span class="bhaumik">bhaumik</span>
+        <span class="func">.is()</span>
+      </h1>
+
+      <TextBlock>
+        <p class="-red">Bhaumik Soni</p>
+        <p class="-gray">Cloud Data Engineer</p>
+        <p class="-gray">Devops Engineer</p>
+      </TextBlock>
+    </div>
+  </section>
+</template>
+
+<script>
+import TextBlock from '../TextBlock.vue'
+
+export default {
+  name: 'IntroScene',
+  components: { TextBlock },
+}
+</script>
